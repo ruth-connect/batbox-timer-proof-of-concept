cc8a8c445427511ceeee31bdee2991fe225feb95
Ruth Mills <ruth.mills@connect-group.com>
2016-09-18 19:43:40 +0100

    Add ControlService.

--- src/main/java/uk/me/ruthmills/batbox/timer/polling/StatusPoller.java
+++ src/main/java/uk/me/ruthmills/batbox/timer/polling/StatusPoller.java
@@ -1,76 +1,19 @@
 package uk.me.ruthmills.batbox.timer.polling;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import uk.me.ruthmills.batbox.timer.model.TemperatureAndHumidityBean;
-import uk.me.ruthmills.batbox.timer.service.BoilerService;
-import uk.me.ruthmills.batbox.timer.service.ThermostatService;
+import uk.me.ruthmills.batbox.timer.service.ControlService;
 
 @Component
 public class StatusPoller {
 	
-	private static final Logger LOGGER = Logger.getLogger(StatusPoller.class);
-	
-	private static final double HEATING_ON_BELOW_THIS_TEMPERATURE = 26.0d;
-	
 	@Autowired
-	private BoilerService boilerService;
+	private ControlService controlService;
 	
-	@Autowired
-	private ThermostatService thermostatService;
-
 	@Scheduled(cron = "*/10 * * * * *")
 	public void checkStatus() {
-		LocalDateTime now = LocalDateTime.now();
-		LOGGER.info("");
-		LOGGER.info("Checking status. Date and time is: " + now);
-		
-		BigDecimal temperature = null;
-		BigDecimal humidity = null;
-		
-		try {
-			TemperatureAndHumidityBean temperatureAndHumidity = thermostatService.readTemperatureAndHumidity();
-			temperature = temperatureAndHumidity.getTemperature();
-			humidity = temperatureAndHumidity.getHumidity();
-			LOGGER.info("Temperature: " + temperature + " C, Humidity: " + humidity + " %");
-		} catch (Exception ex) {
-			LOGGER.error("Thermostat is down. Cannot read temperature");
-		}
-		
-		boolean hotWaterOn = false;
-		int second = now.getSecond();
-		if (second < 30) {
-			LOGGER.info("Hot water is on because we are before 30 seconds");
-			hotWaterOn = true;
-		} else {
-			LOGGER.info("Hot water is off because we are after 30 seconds");
-		}
-
-		boolean heatingOn = false;
-		if (temperature != null) {
-			if (temperature.doubleValue() < HEATING_ON_BELOW_THIS_TEMPERATURE) {
-				LOGGER.info("Heating is on because temperature is less than " + HEATING_ON_BELOW_THIS_TEMPERATURE + " C");
-				heatingOn = true;
-			} else {
-				LOGGER.info("Heating is off because temperature is " + HEATING_ON_BELOW_THIS_TEMPERATURE + " C or greater");
-			}
-		}
-		
-		if (heatingOn) {
-			LOGGER.info("TURNING BOTH HEATING AND HOT WATER ON");
-			boilerService.heatingAndHotWater();
-		} else if (hotWaterOn) {
-			LOGGER.info("TURNING HOT WATER ON AND HEATING OFF");
-			boilerService.hotWaterOnly();
-		} else {
-			LOGGER.info("TURNING BOTH HEATING AND HOT WATER OFF");
-			boilerService.off();
-		}
+		controlService.updateBoilerState();
 	}
 }
---
+++ src/main/java/uk/me/ruthmills/batbox/timer/service/ControlService.java
@@ -0,0 +1,6 @@
+package uk.me.ruthmills.batbox.timer.service;
+
+public interface ControlService {
+
+	public void updateBoilerState();
+}
