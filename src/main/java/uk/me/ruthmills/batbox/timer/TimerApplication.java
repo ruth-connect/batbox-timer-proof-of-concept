552ed2b77d488ffcd97fe463120d33ba8f03285f
Ruth Mills <ruth.mills@connect-group.com>
2016-09-18 12:10:14 +0100

    Create Spring Boot application to run on port 61454.

---
+++ src/main/java/uk/me/ruthmills/batbox/timer/TimerApplication.java
@@ -0,0 +1,12 @@
+package uk.me.ruthmills.batbox.timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimerApplication.class, args);
+	}
+}
