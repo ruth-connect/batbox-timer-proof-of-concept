1fd620e80373b1d5e29dab8f7f14751929d1e8fd
Ruth Mills <ruth.mills@connect-group.com>
2016-09-25 22:26:58 +0100

    Fix JS bug which was firing spurious events.

--- src/main/resources/static/js/batbox.js
+++ src/main/resources/static/js/batbox.js
@@ -1,47 +1,5 @@
 $(document).ready(function() {
 	poll();
-});
-
-function poll() {
-	setTimeout(function() {
-		$.ajax({
-			url: "http://localhost:61454/reportBoilerStatus",
-			success: function(result) {
-				if (result.temperature != null) {
-					$(".temperature").text(result.temperature);
-				}
-				
-				if (result.humidity != null) {
-					$(".humidity").text(result.humidity);
-				}
-				
-				$(".timer").text(result.timerOn ? "ON" : "OFF");
-				if (result.timerOn) {
-					$(".timer").addClass("on");
-				} else {
-					$(".timer").removeClass("on");
-				}
-				
-				$(".hotWater").text(result.hotWaterOn ? "ON" : "OFF");
-				if (result.hotWaterOn) {
-					$(".hotWater").addClass("on");
-				} else {
-					$(".hotWater").removeClass("on");
-				}
-				
-				$(".heating").text(result.heatingOn ? "ON" : "OFF");
-				if (result.heatingOn) {
-					$(".heating").addClass("on");
-				} else {
-					$(".heating").removeClass("on");
-				}
-				
-				$(".targetTemperature").val(result.targetTemperature.replace(".", "/"));
-				
-				poll();
-			}
-		});
-	}, 200);
 	
 	$(".hotWaterOff").click(function() {
 		$.ajax({
@@ -84,4 +42,46 @@ function poll() {
 			url: "http://localhost:61454/setTargetTemperature/" + $(".targetTemperature").val()
 		})
 	});
+});
+
+function poll() {
+	setTimeout(function() {
+		$.ajax({
+			url: "http://localhost:61454/reportBoilerStatus",
+			success: function(result) {
+				if (result.temperature != null) {
+					$(".temperature").text(result.temperature);
+				}
+				
+				if (result.humidity != null) {
+					$(".humidity").text(result.humidity);
+				}
+				
+				$(".timer").text(result.timerOn ? "ON" : "OFF");
+				if (result.timerOn) {
+					$(".timer").addClass("on");
+				} else {
+					$(".timer").removeClass("on");
+				}
+				
+				$(".hotWater").text(result.hotWaterOn ? "ON" : "OFF");
+				if (result.hotWaterOn) {
+					$(".hotWater").addClass("on");
+				} else {
+					$(".hotWater").removeClass("on");
+				}
+				
+				$(".heating").text(result.heatingOn ? "ON" : "OFF");
+				if (result.heatingOn) {
+					$(".heating").addClass("on");
+				} else {
+					$(".heating").removeClass("on");
+				}
+				
+				$(".targetTemperature").val(result.targetTemperature.replace(".", "/"));
+				
+				poll();
+			}
+		});
+	}, 200);
 }
