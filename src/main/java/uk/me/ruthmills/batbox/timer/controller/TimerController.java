bee16d8cc1610eaaf9fdddb0a5cfa76bed7221a8
Ruth Mills <ruth.mills@connect-group.com>
2016-09-25 18:40:36 +0100

    Get TSS working for initial state.

--- src/main/java/uk/me/ruthmills/batbox/timer/controller/TimerController.java
+++ src/main/java/uk/me/ruthmills/batbox/timer/controller/TimerController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import uk.me.ruthmills.batbox.timer.model.BoilerStatusBean;
 import uk.me.ruthmills.batbox.timer.service.ControlService;
@@ -19,11 +20,15 @@ public class TimerController {
 	private ControlService controlService;
 	
 	@RequestMapping({"/", "/index.html"})
-    public String index() {
+    public ModelAndView index() {
 		if(log.isInfoEnabled()) {
 			log.info("TimerController index() invoked");
 		}
-        return "index";
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("boilerStatus", controlService.reportBoilerStatus());
+		modelAndView.setViewName("index");
+        return modelAndView;
     }
 	
 	@RequestMapping(value = "/reportBoilerStatus", method = RequestMethod.GET)
